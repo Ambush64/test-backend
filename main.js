4c335a8c85afdf49e41f96c298712cabcaa196e4
bilal <56475491+Ambush64@users.noreply.github.com>
2023-09-22 22:33:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,12 +4,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const http = require('http')
 const app = express()
-const axios = require("axios")
-const { OAuth2Client } = require('google-auth-library');
-const client = new OAuth2Client('726479389155-15oaapogtf6m287gsmmecbv2c34ledgf.apps.googleusercontent.com');
 const mysql = require("mysql");
-
-
 
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json({ limit: '50mb' }))
@@ -170,127 +165,8 @@ app.post("/orders", (req, res) => {
 });
 
 
-const redirectURL = 'http://localhost:3000';
-
-
-const oAuth2Client = new OAuth2Client(
-  "7147604982-oivo6i2pbn0scp1dcpeqbvt9fegf7ke6.apps.googleusercontent.com",
-  "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",
-  redirectURL
-);
-
-
-app.get('/verify', function (req, res, next) {
-  // verify("eyJhbGciOiJSUzI1NiIsImtpZCI6IjE5MGFkMTE4YTk0MGFkYzlmMmY1Mzc2YjM1MjkyZmVkZThjMmQwZWUiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiVmJqIEd1aXRhciBoZXJvIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FDZzhvY0pONG4zZk54ckxoSlU3czF0XzJ2WVZmT0J5bHR2TVVTTlNjLWdiZ1ZCYz1zOTYtYyIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9yZWFjdC1hdXRoLWZkOTA5IiwiYXVkIjoicmVhY3QtYXV0aC1mZDkwOSIsImF1dGhfdGltZSI6MTY5NDY3ODkzMSwidXNlcl9pZCI6IllIc2IyN2JJOW5manhMdDZFR2xnRTM3ZnF0MTMiLCJzdWIiOiJZSHNiMjdiSTluZmp4THQ2RUdsZ0UzN2ZxdDEzIiwiaWF0IjoxNjk0Njc4OTMxLCJleHAiOjE2OTQ2ODI1MzEsImVtYWlsIjoiZ3VpdGFyaGVyb3ZiakBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJnb29nbGUuY29tIjpbIjExMTM2MjQ2MTkwMDE5Nzc3NjI4MCJdLCJlbWFpbCI6WyJndWl0YXJoZXJvdmJqQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6Imdvb2dsZS5jb20ifX0.jScZRbsriySeG91ThJpb20lfVAT5M7ebPB5A_JyRSnXBZejcZyW2txZB7bMUrqwM3kbs9c7SjAaKs8IfxpsAXyoiz0aK-JFShvCjrUPoViowgM0KZ2J3X-fypQalS8I1_aWI7jStPuFUQNwwDm6CNtg7dS2ZmEun8J3xsvDxpnKeidofXzI4RGzFFZiwwQ2WWQJKMk7tl1J0m-S3T_oX2ORK_iDOfKszCosfgePZ6nHFytfK54G2eY2nR2PSV5YhLik0SE8l6cuNeWQyuMi6JoiaT0xE0rlPsFxiJLLg0K9SGLHRhpXXB-HfFvPPDptbckizpLBAZezJro8sE2QqVg")
-
-  res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
-  res.header("Access-Control-Allow-Credentials", 'true');
-  res.header("Referrer-Policy", "no-referrer-when-downgrade");
-
-  // Generate the url that will be used for the consent dialog.
-  const authorizeUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: 'https://www.googleapis.com/auth/userinfo.profile  openid ',
-    prompt: 'consent',
-    include_granted_scopes: true
-
-  });
-
-  res.json({ url: authorizeUrl })
-
-});
-
-app.post('/getToken', async function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
-  res.header("Access-Control-Allow-Credentials", 'true');
-  res.header("Referrer-Policy", "no-referrer-when-downgrade");
-
-  getRefreshToken(req.body.url)
-
-  let { tokens } = await oAuth2Client.getToken(req.body.url);
-  oAuth2Client.setCredentials(tokens);
-
-  // console.log(tokens)
-  getNewAccessToken(tokens.refresh_token)
-
-  res.json({ tokens })
-});
-
-const getRefreshToken = async (authorizationCode) => {
-  console.log(authorizationCode)
-  const url = `https://oauth2.googleapis.com/token?grant_type=authorization_code&code=${authorizationCode}&client_id=7147604982-oivo6i2pbn0scp1dcpeqbvt9fegf7ke6.apps.googleusercontent.com&client_secret=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&redirect_uri=http://localhost:3000`;
-
-  let payloadForAccessToken = {
-    grant_type: 'refresh_token',
-    refresh_token: authorizationCode,
-    client_id: "7147604982-oivo6i2pbn0scp1dcpeqbvt9fegf7ke6.apps.googleusercontent.com",
-    client_secret: "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",
-  };
-
-
-  console.log(url)
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-    });
-
-    const res = await response.json();
-    console.log('token response: ', res);
-  } catch (error) {
-    console.error('err: ', error);
-  }
-
-
-
-  // try {
-  //   const response = await axios.post(url);
-
-  //   console.log('getRefreshToken', response.data);
-  // } catch (error) {
-  //   console.error('err:', error);
-  // }
-
-};
-
-
-const getNewAccessToken = (body) => {
-  // get new access token using refresh token
-  let payloadForAccessToken = {
-    grant_type: 'refresh_token',
-    refresh_token: body,
-    client_id: "7147604982-oivo6i2pbn0scp1dcpeqbvt9fegf7ke6.apps.googleusercontent.com",
-    client_secret: "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",
-  };
-
-  fetch('https://oauth2.googleapis.com/token', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(payloadForAccessToken),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then((res) => {
-      console.log('new token response: ', res);
-    })
-    .catch((err) => console.log('err: ', err));
-};
-
-
-
 
 http.createServer(app).listen(port, () => {
   console.log(`server started at port ${port}`)
 })
 
-
-
-// id
-// 7147604982-oivo6i2pbn0scp1dcpeqbvt9fegf7ke6.apps.googleusercontent.com
-// secret
-// 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
